77539d3516665bd8fb299151905a6860876cfa71
1F10220273 <s1F102202730@iniad.org>
2024-08-26 02:11:57 +0900

    change file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
-import Top from './components/user/top'
+import Top from './components/top'
 
 export default function Home() {
   const router= useRouter()
